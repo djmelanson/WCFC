f89f2c6b46c05a9999a352aae888408d3ccc0dc7
root <david.melanson20@gmail.com>
2020-11-16 16:45:55 +0000

    test

---
+++ script.js
@@ -0,0 +1,12 @@
+const gw1Date = document.querySelector("#gw1-date");
+const gw1Home = document.querySelector("#gw1-home");
+const gw1Away = document.querySelector("#gw1-away");
+const gw1Field = document.querySelector("#gw1-field");
+
+const wcfc = "Worcester County FC";
+const gameWeek1 = {date: date(2020, 8, 19, 11), homeTeam: "North Shore FC", awayTeam: wcfc, field: "Harry Della Russo Stadium, Revere"};
+
+let gw1Date = gameWeek1.date;
+let gw1Home = gameWeek1.homeTeam;
+let gw1Away = gameWeek1.awayTeam;
+let gw1Field = gameWeek1.field;
